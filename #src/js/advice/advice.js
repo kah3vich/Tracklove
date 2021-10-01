ba545de3e82d6e0544bff425f42edb93f71e266e
kah3vich <70528062+kah3vich@users.noreply.github.com>
2021-10-01 20:02:49 +0300

    file

--- #src/js/advice/advice.js
+++ #src/js/advice/advice.js
@@ -6,6 +6,15 @@ var adviceBlogSlider = new Swiper(".adviceBlogSlider", {
         nextEl: ".advice__blog-slider .swiper-controls .swiper-button-next",
         prevEl: ".advice__blog-slider .swiper-controls .swiper-button-prev",
     },
+    breakpoints: {
+        750: {
+            slidesPerView: 2,
+        },
+        0: {
+            slidesPerView: 1,
+            spaceBetweenSlides: 10
+        }
+    }
 });
 
 //! ---------------------------------------------------------------------------
